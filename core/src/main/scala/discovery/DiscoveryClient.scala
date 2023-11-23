9b3e60caadf71edcfd7971b88305f540d90f7410
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-11-23 22:31:23 +0100

    AndJson to make sure we can save the json

--- core/src/main/scala/discovery/DiscoveryClient.scala
+++ core/src/main/scala/discovery/DiscoveryClient.scala
@@ -1,7 +1,8 @@
 package discovery
 
 import cats.effect.kernel.Concurrent
-import io.circe.Decoder
+import discovery.DiscoveryClient.AsJson
+import io.circe.{Decoder, Encoder, Json}
 import org.http4s.{EntityDecoder, Request}
 import org.http4s.client.{Client => Http4sClient}
 import org.http4s.implicits._
@@ -15,5 +16,19 @@ class DiscoveryClient[F[_]: Concurrent](client: Http4sClient[F]) {
       Request[F](uri = uri"https://discovery.googleapis.com/discovery/v1/apis"))
 
   def getDiscovery(item: DiscoveryCollection.Item) =
-    client.expectOption[Discovery](Request[F](uri = item.discoveryRestUrl))
+    client.expectOption[AsJson[Discovery]](Request[F](uri = item.discoveryRestUrl))
+}
+
+object DiscoveryClient {
+  case class AsJson[A](value: A, json: Json)
+  object AsJson {
+    implicit def decoder[A](implicit d: Decoder[A]): Decoder[AsJson[A]] =
+      Decoder.instance(c =>
+        for {
+          json <- c.as[Json]
+          value <- json.as[A](d)
+        } yield AsJson(value, json))
+
+    implicit def encoder[A]: Encoder[AsJson[A]] = Encoder.encodeJson.contramap(_.json)
+  }
 }
