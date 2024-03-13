ac8c9704b149f727d63806949b4fe4996b63c16c
Erlend Hamnaberg <erlend@hamnaberg.net>
2024-03-13 09:27:11 +0100

    remove debug println

--- core/src/test/scala/discovery/DiscoveryTest.scala
+++ core/src/test/scala/discovery/DiscoveryTest.scala
@@ -9,8 +9,7 @@ class DiscoveryTest extends munit.FunSuite {
     val resource = getClass.getResourceAsStream("/bigquery.json")
     val decoded = jawn.decodeChannel[Discovery](Channels.newChannel(resource))
     val discovery = decoded.fold(err => fail(err.getMessage), identity)
-    val files = Codegen.generateFromDiscovery("bq", discovery)
-    files.find(_.name == "Binding").foreach(println)
+    Codegen.generateFromDiscovery("bq", discovery)
   }
 
   test("can decode storage") {
